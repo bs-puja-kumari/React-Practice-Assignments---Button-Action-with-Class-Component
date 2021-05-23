ec7f9f38db63c4455d0c790afa9da6a51e75134f
bs-puja-kumari <83964038+bs-puja-kumari@users.noreply.github.com>
2021-05-23 22:41:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ class App extends Component {
 	
     constructor(props) {
 		super(props);
-	
+	this.data="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
 	};
 	    
  
     render() {
     	return(
     		<div id="main">
-		<button id="click" onclick={()=>{<p id="para">{this.state.data}</p>}}>click</button>
+		<button id="click" onclick={()=><p id="para">{this.data}</p>}>click</button>
                 
 
 				{ /* Do not remove this main div!! */ }
