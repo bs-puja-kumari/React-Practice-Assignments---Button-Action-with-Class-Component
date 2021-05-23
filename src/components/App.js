57dfc326bab3991447815dd95cf62464a569e13b
bs-puja-kumari <83964038+bs-puja-kumari@users.noreply.github.com>
2021-05-23 21:35:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,22 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+	const[data,setdata]=usestate("p")
     constructor(props) {
 		super(props);
+	    
 	};
-
+	    
+ apple(){
+  setdata("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+ 
+ }
     render() {
     	return(
     		<div id="main">
-		<button id="click" onclick={()=> {<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-</p>}}
+		<button id="click" onclick={()=>{apple()}}> on click</button>
+                <p id="para">{data}<p>
+
 				{ /* Do not remove this main div!! */ }
     		</div>
     	);
